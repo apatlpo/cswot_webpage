cd3286338b21170ee7afa246358b69b062a40964
Aurélien Ponte <aurelien.junk@gmail.com>
2023-03-09 16:41:53 +0100

    init with dummy scripts

---
+++ job.zsh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+# load conda environment
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/aponte/.miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/aponte/.miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/aponte/.miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/aponte/.miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+conda activate pynsitu
+
+
+
+# temporary: converts notebook to python script
+jupyter nbconvert --to python generate_html.ipynb
+
+# execute script
+python generate_html.py 
+
+
